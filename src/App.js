1794cc5d038244617e6cf7235660fd693c49ac70
Kevin <knphillips01@gmail.com>
2020-01-22 22:05:46 -0600

    met MVP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import React, {useState, useEffect} from "react";
-import axios from 'axios';
+import React from "react";
 import "./App.css";
+import NasaCard from "./components/NasaCard";
 
 function App() {
-  
-  const[data, setData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get('https://api.nasa.gov/planetary/apod?api_key=5iN3mHKJ5c9qFXvEIgfC38JQ1CTgDUhE7S3fhf8j')
-      .then(response => {
-        console.log(response); 
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  },[])  
-  
   return (
     <div className="App">
-      
+     <NasaCard />
     </div>
   );
 }
