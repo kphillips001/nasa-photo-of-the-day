5aa7bcf2ed99734cc11910d72173f3e6d93bac7e
Kevin <knphillips01@gmail.com>
2020-01-23 17:46:45 -0600

    pushing new style changes

--- src/components/NasaCardInfo.js
+++ src/components/NasaCardInfo.js
@@ -1,16 +1,23 @@
 import React from "react";
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button,CardHeader
+  } from 'reactstrap';
 
 const NasaCardInfo = props => {
     return (
-        <div>
-            <h2>Title: {props.data.title}</h2>
-            <p>Description: {props.data.explanation}</p>
-            <img src={props.data.url} alt='Nasa'/>
-            <div className="bottom">
-                <p>Photographer: {props.data.copyright}</p>
-                <p>Current Date: {props.data.date}</p>
-            </div>
-        </div>
+        <Card>
+            <CardHeader>
+                <h1>{props.data.title}</h1>
+            </CardHeader>  
+            <CardBody>           
+                <CardText>{props.data.explanation}</CardText>
+                <img src={props.data.url} alt='Nasa'/>
+                <CardText>Photographer: {props.data.copyright}</CardText>
+                <CardText>Current Date: {props.data.date}</CardText>
+            </CardBody> 
+        </Card>
+        
     )
 }
 
