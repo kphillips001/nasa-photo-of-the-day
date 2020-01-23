fed59921e4b3b46b53621adf0d0da00e77a1da69
Kevin <knphillips01@gmail.com>
2020-01-22 20:52:52 -0600

    moved state back up to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
-import Card from './components/Card'; 
 
 function App() {
+  
+  const[data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://api.nasa.gov/planetary/apod?api_key=5iN3mHKJ5c9qFXvEIgfC38JQ1CTgDUhE7S3fhf8j')
+      .then(response => {
+        console.log(response); 
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },[])  
+  
   return (
     <div className="App">
-      <Card />
+      
     </div>
   );
 }
